357f4ae50c8bdf6c350f368ac88d912f18cc98da
Сергей Ткачев <egrbrn@mail.ru>
2019-11-21 03:15:14 +0600

    Practice OpenVINO (NOT READY)

---
+++ samples/practice2_TKACHEV.py
@@ -0,0 +1,52 @@
+﻿"""
+
+Inference engine detector sample
+ 
+"""
+import sys
+import cv2
+import argparse
+import logging as log
+sys.path.append('../src')
+from ie_detector import InferenceEngineDetector
+import time
+def build_argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pX', '--pathXML', help = 'path to input XML', type = str)
+    parser.add_argument('-pB', '--pathBin', help = 'path to input bin', type = str)
+    parser.add_argument('-t', '--type', help = 'type of device', type = str)
+    parser.add_argument('-l', '--pathLib', help = 'path to library', type = str)  
+    parser.add_argument('-i', '--image', help = 'path to image', type = str)
+    parser.add_argument('-v',"--video", help="path to video file", type = str)
+    return parser
+
+def main():
+    log.basicConfig(format = "[%(levelname)s] %(message)s", level = log.INFO, 
+                               stream = sys.stdout)
+    log.info("Hello object detection!")
+    args = build_argparse().parse_args()
+    ied = InferenceEngineDetector(weightsPath = args.pathBin, configPath = args.pathXML,
+                 device = args.type, extension = args.pathLib)
+    if args.video:
+        cap = cv2.VideoCapture(args.video)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                frame_resized = cv2.resize(frame,(300,300))
+                final_image = ied.detect(frame_resized)
+                cv2.imshow("Post-processing image", final_image)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+            else: 
+                break
+    elif args.image:
+        cap = cv2.imread(args.image)        
+        final_image = ied.detect(cap)
+        cv2.imshow("Post-processing image", final_image)
+        cv2.waitKey(0)
+    cap.release()
+    cv2.destroyAllWindows()
+    return 
+
+if __name__ == '__main__':
+    sys.exit(main()) 
--- src/ie_detector.py
+++ src/ie_detector.py
@@ -4,39 +4,70 @@ Inference engine detector
  
 """
 import cv2
+import math
 import numpy as np
+import logging as log
 from openvino.inference_engine import IENetwork, IECore
+from time import time
+#Labels of network.
+classNames = { 0: 'background',
+    1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat',
+    5: 'bottle', 6: 'bus', 7: 'car', 8: 'cat', 9: 'chair',
+    10: 'cow', 11: 'diningtable', 12: 'dog', 13: 'horse',
+    14: 'motorbike', 15: 'person', 16: 'pottedplant',
+    17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor' }
 
 class InferenceEngineDetector:
     def __init__(self, weightsPath = None, configPath = None,
                  device = 'CPU', extension = None):
-        #
-        # Add your code here
-        #
-        
-        return
+        self.ie = IECore()
+        if extension:
+            self.ie.add_extension(extension, 'CPU')
+        self.net = IENetwork(model=configPath, weights=weightsPath)
+        self.exec_net = self.ie.load_network(network=self.net, device_name='CPU')
 
-    def draw_detection(self, detections, img):
-    
-        #
-        # Add your code here
-        #
-        
+    def draw_detection(self, detections, img):    
+        cols = img.shape[1] 
+        rows = img.shape[0]
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.5:
+                class_id = int(detections[0, 0, i, 1]) 
+                xLeftTop = int(detections[0, 0, i, 3] * cols) 
+                yLeftTop = int(detections[0, 0, i, 4] * rows)
+                xRightBottom = int(detections[0, 0, i, 5] * cols)
+                yRightBottom = int(detections[0, 0, i, 6] * rows)
+                cv2.rectangle(img, (xLeftTop, yLeftTop), (xRightBottom, yRightBottom), (0, 255, 0), 3)
+                log.info(str(classNames[class_id]) + " conf.: " + str(confidence))
+                cv2.putText(img, str(classNames[class_id]) + " " + str (round(confidence, 2)), (xLeftTop, yLeftTop), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2, cv2.LINE_AA)
         return img
 
     def _prepare_image(self, image, h, w):
-    
-        #
-        # Add your code here
-        #
-        
+        image = cv2.resize(image, dsize = (w,h))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image.transpose((2, 0, 1)) 
+        image = np.expand_dims(image, axis = 0)
         return image
         
     def detect(self, image):
-    
-        #
-        # Add your code here
-        #
-        detection = None
-        
+        input_blob = next(iter(self.net.inputs))
+        out_blob = next(iter(self.net.outputs))
+        n, c, h, w = self.net.inputs[input_blob].shape
+        blob = self._prepare_image(image, h, w)
+        times = []
+        t0_total = time()
+        # for getting FPS 
+        number_iter = 1
+        for i in range(0,number_iter):
+            t0 = time()
+            output = self.exec_net.infer(inputs = {input_blob: blob})
+            t1 = time()
+            times.append(t1 - t0)
+        t1_total = time()
+        latency = np.median(times)
+        fps = number_iter/(t1_total - t0_total)
+        log.info("FPS: " + str(fps))
+        log.info("Latency: " + str(latency))
+        output = output[out_blob]
+        detection = self.draw_detection(detections = output, img = image)        
         return detection
